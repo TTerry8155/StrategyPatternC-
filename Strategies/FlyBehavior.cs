b74e57981cff339584a4907cd9f52410cb6573b8
TTerry8155 <tterry8155@gmail.com>
2017-07-13 11:16:05 -0500

    Implemented FlyBehavior

---
+++ Strategies/FlyBehavior.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StrategyPattern.Strategies
+{
+    public class FlyBehavior : IBehaviorStrategy
+    {
+        public void ExecuteBehavior()
+        {
+            Console.WriteLine("WOOSH WOOSH! The animal has taken off.");
+        }
+    }
+}
