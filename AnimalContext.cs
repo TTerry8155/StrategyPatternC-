ff3eb82bca5d13f9e4a6923dc763c1678165b9fb
TTerry8155 <tterry8155@gmail.com>
2017-07-13 11:21:59 -0500

    Implemented WalkBehavior and DescribeAnimal Functionality

--- AnimalContext.cs
+++ AnimalContext.cs
@@ -6,9 +6,16 @@ namespace StrategyPattern
 {
     public class AnimalContext
     {
+        public string Name { get; set; }
+
         public void PreformBehavior()
         {
 
+        }
+
+        public void DescribeAnimal()
+        {
+            Console.WriteLine("This animal name is {0}.", Name);
         }
     }
 }
---
+++ Strategies/WalkBehavior.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StrategyPattern.Strategies
+{
+    public class WalkBehavior : IBehaviorStrategy
+    {
+        public void ExecuteBehavior()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
