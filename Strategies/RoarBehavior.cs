9fdab4768ecf584f2a85a31e8e83cc2149a20c54
TTerry8155 <tterry8155@gmail.com>
2017-07-13 11:12:52 -0500

    Added Behavior Strategy and implemented roar behavior

---
+++ Strategies/RoarBehavior.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StrategyPattern.Strategies
+{
+    public class RoarBehavior : IBehaviorStrategy
+    {
+        public void ExecuteBehavior()
+        {
+            Console.WriteLine("ROARRRRRRRRRRRRRRRRRR!");
+        }
+    }
+}
